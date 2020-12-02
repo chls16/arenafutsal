eefc760e07d5fe095ae14a98e6113d270e3e759f
Chelsea <chelsea.rputri@gmail.com>
2020-12-03 02:39:57 +0700

    Update page.php

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -2,19 +2,49 @@
 
 class Page extends CI_Controller {
 
+	public function __construct(){
+	
+			parent::__construct();
+        $this->load->helper(array('form','url'));
+		$this ->load ->model('m_account');
+        $this ->load ->model('m_admin');
+		if($this->session->userdata('id') != TRUE ){
+            $this->session->set_flashdata('notif','LOGIN GAGAL USERNAME ATAU PASSWORD ANDA SALAH !');
+            redirect('akun');
+        };
+
+      }
+
 	public function booking()
 	{
-		$this->load->view('page/booking');
+		$user=$this->m_account->getuser($this->session->userdata('id'));
+		$lapangan=$this->m_account->lapangan();
+		$data = array(
+			'user' => $user,
+			'lapangan' => $lapangan,
+			'panelbody' =>'page/booking',
+		);
+		$this->load->view('panelbody', $data);
 	}
 
 	public function kontak()
 	{
-		$this->load->view('kontak');
+		$user=$this->m_account->getuser($this->session->userdata('id'));
+		$data = array(
+			'user' => $user,
+			'panelbody' =>'kontak',
+		);
+		$this->load->view('panelbody', $data);
 	}
-
+	
 	public function about()
 	{
-		$this->load->view('page/about');
+		$user=$this->m_account->getuser($this->session->userdata('id'));
+		$data = array(
+			'user' => $user,
+			'panelbody' =>'page/about',
+		);
+		$this->load->view('panelbody', $data);
 	}
 }
 
