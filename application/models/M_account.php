0079784d367c4f6d2b4d9130f03009d8626863f2
Zainal Abidin <zainalabidin170197@gmail.com>
2021-01-18 21:38:37 +0700

    update booking

--- application/models/M_account.php
+++ application/models/M_account.php
@@ -80,7 +80,7 @@
 */
 		function add_booking($table, $data){
 			$this->db->set($data);
-			$this->db->insert('jadwal',$data);
+			$this->db->insert('transaksi',$data);
 		}
 
 		function getuser($id){
